1f64c36bc623b563b35650cf87bf91d1a9396130
jboude <boudejerome@gmail.com>
2024-05-14 16:06:11 +0200

    [YAMS-019] add basic functions to handle the game logic

---
+++ yams-front/src/config/combinaisons.js
@@ -0,0 +1,14 @@
+export const combinaisons = [
+    {
+        name: "Four of a Kind",
+        rewardAmount: 1
+    },
+    {
+        name: "Large Straight",
+        rewardAmount: 1
+    },
+    {
+        name: "Yahtzee",
+        rewardAmount: 1
+    }
+]
