211f430633ce0a51c2c9ec74ece4f26d8a357bb4
Merce <88335955+Merce213@users.noreply.github.com>
2024-05-15 09:52:58 +0200

    [YAMS-006] Login page for the back office admin (#4)

--- yams-front/src/App.jsx
+++ yams-front/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import LayoutNav from "./components/LayoutNav";
+import Login from "./pages/Login";
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/login" element={<Login />} />
 			<Route
 				path="*"
 				element={
