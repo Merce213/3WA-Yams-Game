d11fb5552b4caf41e8dd46abb3eb48a51a9fd5d8
Merce <88335955+Merce213@users.noreply.github.com>
2024-05-15 14:18:40 +0200

    [YAMS-005] Win page + responsive, add a button component, add assets image for win page (#7)

--- yams-front/src/App.jsx
+++ yams-front/src/App.jsx
@@ -3,13 +3,21 @@ import Home from "./pages/Home";
 import LayoutNav from "./components/LayoutNav";
 import Login from "./pages/Login";
 import GameOver from "./pages/GameOver";
+import Win from "./pages/Win";
 
 const App = () => {
-
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/login" element={<Login />} />
+			<Route
+				path="/win"
+				element={
+					<LayoutNav>
+						<Win />
+					</LayoutNav>
+				}
+			/>
 			<Route
 				path="/gameover"
 				element={
