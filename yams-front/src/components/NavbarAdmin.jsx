ea6b1cbb7a7d4540ef1aa359b2796a6d964cf68b
Merce <nassim-bgb69@gmx.fr>
2024-05-16 17:29:16 +0200

    [YAMS-018] fix styles on navbar admin

--- yams-front/src/components/NavbarAdmin.jsx
+++ yams-front/src/components/NavbarAdmin.jsx
@@ -5,8 +5,8 @@ import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 const NavbarAdmin = () => {
 	return (
 		<header className="bg-yams-purple text-white fixed top-0 z-50 w-full">
-			<nav className="flex container mx-auto items-center justify-between px-3 py-2">
-				<NavLink to="/" className="font-yams-title text-xl">
+			<nav className="flex items-center justify-between px-4 py-2">
+				<NavLink to="/" className="font-yams-title text-xl lg:ps-16">
 					Yams
 				</NavLink>
 				<div className="flex items-center gap-2">
