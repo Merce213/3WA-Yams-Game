2b529b8525a48cdecfa1460448bea10ab17210fd
jboude <boudejerome@gmail.com>
2024-05-17 15:55:38 +0200

    [YAMS-028] Add missing credits

--- yams-front/src/pages/Credits.jsx
+++ yams-front/src/pages/Credits.jsx
@@ -15,6 +15,8 @@ const Credits = () => {
                             <ul className="text-base mb-8 pl-4 list-disc list-none text-center">
                                 <li><a href="https://azilla.itch.io/simple-buttons-gui-kit" target="_blank" rel="noreferrer" className="text-blue-300 hover:text-blue-500">Trophee</a></li>
                                 <li><a href="https://penzilla.itch.io/vector-icon-pack" target="_blank" rel="noreferrer" className="text-blue-300 hover:text-blue-500">Skull</a></li>
+                                <li><a href="https://www.pngall.com/dice-png/download/3073" target="_blank" rel="noreferrer" className="text-blue-300 hover:text-blue-500">Dice</a></li>
+                                <li><a href="https://www.pngall.com/ribbon-banner-png/download/132001" target="_blank" rel="noreferrer" className="text-blue-300 hover:text-blue-500">Ribbon Banner</a></li>
                                 <li>The background image was generated with <a href="https://creator.nightcafe.studio/" target="_blank" rel="noreferrer" className="text-blue-300 hover:text-blue-500">Nightcafe Studio</a></li>
                             </ul>
                             <p className="text-lg mt-10 mb-2">Github repository:</p>
